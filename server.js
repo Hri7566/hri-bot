3135ae3975ff3f9805af4bdc4125ac3597086f96
hri7566 <hri7566@gmail.com>
2020-03-09 19:28:52 -0400

    fixing heroku shit

--- server.js
+++ server.js
@@ -11097,6 +11097,6 @@ var http = require("http");
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Unable to shade polygon normals');
-}).listen($PORT);
+}).listen(8081);
 
 console.log("HTTP Host Established");
